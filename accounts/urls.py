63ba5c8286ae8e4d2687da6997e907b42af8dec7
april <martinezapril56@yahoo.com>
2018-10-18 13:42:23 -0400

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import url
 from . import views
-# from django.contrib.auth.views import (
-#     login, logout, password_reset, password_reset_done, password_reset_confirm,
-#     password_reset_complete
-# )
+from django.contrib.auth.views import (
+    login, logout, password_reset, password_reset_done, password_reset_confirm,
+    password_reset_complete
+)
 
-from accounts.views import reset_password_request_token, reset_password_confirm
 
-app_name="accounts"
 
 urlpatterns = [
+    url(r'^$', views.home),
     url(r'^login/$', login,
         {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout/$', logout,
@@ -21,7 +20,7 @@ urlpatterns = [
     url(r'^change-password/$', views.change_password, name='change_password'),
 
     url(r'^reset-password/$', password_reset, {'template_name': 'accounts/reset_password.html', 'post_reset_redirect':
- 'accounts:password_reset_done', 'email_template_name': 'accounts/reset_password_email.html'}, name='reset_password'),
+'accounts:password_reset_done', 'email_template_name': 'accounts/reset_password_email.html'}, name='reset_password'),
 
     url(r'^reset-password/done/$', password_reset_done, {'template_name': 'accounts/reset_password_done.html'},
 name='password_reset_done'),
