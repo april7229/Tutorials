60ccecc2228f132f5694befa60f6ec18502c53fb
april <martinezapril56@yahoo.com>
2018-10-19 15:56:45 -0400

    working on new things

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import (
-    login, logout, password_reset, password_reset_done, password_reset_confirm,
-    password_reset_complete
+    login, logout, password_reset, password_reset_done, password_reset_confirm,password_reset_complete
 )
 app_name="tutorial"
 urlpatterns = [
